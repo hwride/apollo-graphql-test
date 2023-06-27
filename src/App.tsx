50d598075ce78b616cdcc050b0e4a2638a44e3de
Hugo Wride <hugowride@gmail.com>
2023-06-27 15:50:39 +0100

    Add basic use of Apollo client with React.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {DisplayLocations} from "./DisplayLocations.tsx";
+
+const client = new ApolloClient({
+    uri: 'https://flyby-router-demo.herokuapp.com/',
+    cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
       <div>
-          App
+          <DisplayLocations />
       </div>
-    </>
   )
 }
 
-export default App
+function AppWithProviders() {
+    return <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+}
+
+export default AppWithProviders
