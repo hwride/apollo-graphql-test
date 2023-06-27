63c9432e1cbba2988c7434861a95a31b9c774c2c
Hugo Wride <hugowride@gmail.com>
2023-06-27 15:45:25 +0100

    Add very basic node script calling GraphQL endpoint.

---
+++ node/apollo-test.mjs
@@ -0,0 +1,27 @@
+import ApolloClientPackage from '@apollo/client';
+
+const { ApolloClient, InMemoryCache, ApolloProvider, gql } = ApolloClientPackage;
+
+main()
+
+async function main() {
+    const client = new ApolloClient({
+        uri: 'https://flyby-router-demo.herokuapp.com/',
+        cache: new InMemoryCache(),
+    });
+
+    const result = await client
+        .query({
+            query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `
+        })
+    console.log(JSON.stringify(result, null, 2))
+}
