f61b4f6794e6d5de3a67fabe4da28d8f204eaebc
Hugo Wride <hugowride@gmail.com>
2023-06-30 15:14:29 +0100

    Add previous GraphQL code back in.

--- src/examples/Queries.tsx
+++ src/examples/Queries.tsx
@@ -1,3 +1,48 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+  useQuery,
+} from '@apollo/client'
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`
+
 export function Queries() {
-    return <div>Queries</div>
+  const client = new ApolloClient({
+    uri: 'https://flyby-router-demo.herokuapp.com/',
+    cache: new InMemoryCache(),
+  })
+  return (
+    <ApolloProvider client={client}>
+      <QueriesInner />
+    </ApolloProvider>
+  )
+}
+
+function QueriesInner() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error : {error.message}</p>
+
+  return data.locations.map(({ id, name, description, photo }: any) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ))
 }
