9884ddc25828930bd8daa625649e8abcfa4ce396
Hugo Wride <hugowride@gmail.com>
2023-06-30 15:17:09 +0100

    Make Apollog getting started e.g. its own e.g.

--- src/exampleConfig.ts
+++ src/exampleConfig.ts
@@ -1,8 +1,13 @@
-import {Queries} from "./examples/Queries.tsx";
-
+import { ApolloDocsGettingStarted } from './examples/ApolloDocsGettingStarted.tsx'
+import { Queries } from './examples/Queries.tsx'
 
 export type ConfigEntry = { id: string; label: string; Component: React.FC }
 export const componentConfig: ConfigEntry[] = [
+  {
+    id: 'apollo_docs_getting_started',
+    label: 'Apollo docs getting started',
+    Component: ApolloDocsGettingStarted,
+  },
   {
     id: 'queries',
     label: 'Queries',
