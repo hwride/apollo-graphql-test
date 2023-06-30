95a025dd908789f65290f5eacb2df382eb698e1f
Hugo Wride <hugowride@gmail.com>
2023-06-30 16:23:20 +0100

    Add some query notes.

--- src/examples/Queries.tsx
+++ src/examples/Queries.tsx
@@ -8,12 +8,14 @@ import {
   useQuery,
 } from '@apollo/client'
 import { useState } from 'react'
+import { BorderButton } from '../components/Button.tsx'
 import { ControlGrid } from '../components/ControlGrid.tsx'
 import {
   BoolLabelledSelect,
   LabelledSelect,
 } from '../components/LabelledSelect.tsx'
 import { Page } from '../components/Page.tsx'
+import { PageParagraph } from '../components/PageParagraph.tsx'
 
 const GET_LOCATION = gql`
   query GetLocation($locationId: ID!) {
@@ -51,20 +53,26 @@ export function Queries() {
 
 function QueriesInner() {
   const [locationId, setLocationId] = useState('loc-1')
+  const [pollInterval, setPollInterval] = useState(0)
   const [fetchPolicy, setFetchPolicy] = useState('cache-first')
   const [nextFetchPolicy, setNextFetchPolicy] = useState('cache-first')
   const [notifyOnNetworkStatusChange, setNotifyOnNetworkStatusChange] =
     useState(true)
-  const { loading, error, data, networkStatus } = useQuery(GET_LOCATION, {
-    notifyOnNetworkStatusChange,
-    fetchPolicy: fetchPolicy as WatchQueryFetchPolicy, // Used for first execution
-    nextFetchPolicy: nextFetchPolicy as WatchQueryFetchPolicy, // Used for subsequent executions
-    variables: { locationId },
-  })
+  const { loading, error, data, networkStatus, refetch } = useQuery(
+    GET_LOCATION,
+    {
+      pollInterval,
+      notifyOnNetworkStatusChange,
+      fetchPolicy: fetchPolicy as WatchQueryFetchPolicy, // Used for first execution
+      nextFetchPolicy: nextFetchPolicy as WatchQueryFetchPolicy, // Used for subsequent executions
+      variables: { locationId },
+    }
+  )
 
   const networkStatusName = NetworkStatus[networkStatus]
   console.log(
-    `loading: ${loading}, networkStatus: ${networkStatusName} (${networkStatus})`
+    `loading: ${loading}, networkStatus: ${networkStatusName} (${networkStatus}), data: %o`,
+    data
   )
 
   let resultContent
@@ -91,6 +99,26 @@ function QueriesInner() {
 
   return (
     <Page title="Queries">
+      <PageParagraph>
+        This is a test of different Apollo Client query options.
+      </PageParagraph>
+      <PageParagraph>
+        When <code>notifyOnNetworkStatusChange</code> is false the component
+        will not re-render when fetching with an update <code>loading</code> or{' '}
+        <code>networkStatus</code> attribute for either polling or refetch.
+      </PageParagraph>
+      <PageParagraph>
+        Note when <code>notifyOnNetworkStatusChange</code> is true and the
+        component re-renders while data is being fetched, the old data remains
+        available in <code>data</code>.
+      </PageParagraph>
+      <PageParagraph>
+        But also notice that regardless of the value of{' '}
+        <code>notifyOnNetworkStatusChange</code> the component will re-render
+        and <code>loading</code> will change when you submit a new ID. This
+        seems to suggest the disabling of re-renders by default for subsequent
+        fetches only applies to objects already requested.
+      </PageParagraph>
       <ControlGrid>
         <LabelledSelect
           label="Location ID"
@@ -103,6 +131,19 @@ function QueriesInner() {
           <option value="loc-3">loc-3</option>
           <option value="loc-4">loc-4</option>
           <option value="loc-5">loc-5</option>
+        </LabelledSelect>
+        <LabelledSelect
+          label={
+            <>
+              <code>pollInterval</code> (ms)
+            </>
+          }
+          selectClassName="font-mono"
+          value={String(pollInterval)}
+          onOptionChange={(newPollStr) => setPollInterval(Number(newPollStr))}
+        >
+          <option value="0">0</option>
+          <option value="500">500</option>
         </LabelledSelect>
         <BoolLabelledSelect
           label={<code>notifyOnNetworkStatusChange</code>}
@@ -125,6 +166,12 @@ function QueriesInner() {
         >
           {fetchPolicies}
         </LabelledSelect>
+        <BorderButton
+          className="col-span-2 my-2 justify-self-center"
+          onClick={() => refetch()}
+        >
+          <code>refetch()</code>
+        </BorderButton>
       </ControlGrid>
       {resultContent}
     </Page>
