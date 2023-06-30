94957a7c2b04c2168157e143ea6f33fb78134fec
Hugo Wride <hugowride@gmail.com>
2023-06-30 15:30:38 +0100

    Start on queries example.

--- src/examples/Queries.tsx
+++ src/examples/Queries.tsx
@@ -6,9 +6,9 @@ import {
   useQuery,
 } from '@apollo/client'
 
-const GET_LOCATIONS = gql`
-  query GetLocations {
-    locations {
+const GET_LOCATION = gql`
+  query GetLocation($locationId: ID!) {
+    location(id: $locationId) {
       id
       name
       description
@@ -30,19 +30,31 @@ export function Queries() {
 }
 
 function QueriesInner() {
-  const { loading, error, data } = useQuery(GET_LOCATIONS)
+  const { loading, error, data } = useQuery(GET_LOCATION, {
+    variables: { locationId: 'loc-1' },
+  })
 
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error : {error.message}</p>
+  let resultContent
+  if (loading) resultContent = <p>Loading...</p>
+  else if (error) resultContent = <p>Error : {error.message}</p>
+  else {
+    const { id, name, description, photo } = data.location
+    resultContent = (
+      <div key={id}>
+        <h3>{name}</h3>
+        <img
+          width="400"
+          height="250"
+          alt="location-reference"
+          src={`${photo}`}
+        />
+        <br />
+        <b>About this location:</b>
+        <p>{description}</p>
+        <br />
+      </div>
+    )
+  }
 
-  return data.locations.map(({ id, name, description, photo }: any) => (
-    <div key={id}>
-      <h3>{name}</h3>
-      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
-      <br />
-      <b>About this location:</b>
-      <p>{description}</p>
-      <br />
-    </div>
-  ))
+  return <>{resultContent}</>
 }
