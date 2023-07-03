645adf80812c614f11e7ac861e88aa7ff9252ec3
Hugo Wride <hugowride@gmail.com>
2023-07-03 11:34:39 +0100

    Disable strict mode to improve debugging/logging for Apollo.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  // Disabling strict mode because the double rendering causing some confusing
+  // logging, e.g. onQueryUpdated was logging the same query twice even with
+  // React DevTools strict mode double logging disabled.
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+)
