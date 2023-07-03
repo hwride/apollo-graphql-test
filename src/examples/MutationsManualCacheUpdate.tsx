9871891643fb795ff7e4a1938d8be72eebc1a0c2
Hugo Wride <hugowride@gmail.com>
2023-07-03 10:27:21 +0100

    Add updating of list in cache using update after mutation.

--- src/examples/MutationsManualCacheUpdate.tsx
+++ src/examples/MutationsManualCacheUpdate.tsx
@@ -3,7 +3,9 @@ import { useState } from 'react'
 import { Books } from '../components/Books.tsx'
 import { BorderButton } from '../components/ui/Button.tsx'
 import { ControlGrid } from '../components/ui/ControlGrid.tsx'
+import { H2Inline } from '../components/ui/Headings.tsx'
 import { BoolLabelledSelect } from '../components/ui/LabelledSelect.tsx'
+import { Link } from '../components/ui/Link.tsx'
 import { Page } from '../components/ui/Page.tsx'
 import { PageParagraph } from '../components/ui/PageParagraph.tsx'
 
@@ -50,10 +52,35 @@ function getNextBookTitleSuffix() {
 
 export function MutationsManualCacheUpdate() {
   const { data: booksData, loading: getBooksLoading } = useQuery(GET_BOOKS)
-  const [refetchGetBooks, setRefetchGetBooks] = useState(true)
+  const [refetchGetBooks, setRefetchGetBooks] = useState(false)
   const refetchQueries = refetchGetBooks ? [GET_BOOKS] : undefined
   const [addBook, { data, loading, error, reset }] = useMutation(ADD_BOOK, {
     refetchQueries,
+    // Use update to manually modify the cache after the mutation completes and
+    // add the new book.
+    update(cache, { data: { addBook } }) {
+      cache.modify({
+        fields: {
+          books(existingBooks = []) {
+            // The book will have already been created automatically from the data returned from the mutation.
+            // So we just want to get a reference to it and add it to the books list in the cache.
+            // cache.writeFragment can be used to get a reference to an object that already exists.
+            const newBookRef = cache.writeFragment({
+              data: addBook.book,
+              fragment: gql`
+                fragment NewBook on Book {
+                  id
+                  title
+                  author
+                }
+              `,
+            })
+            console.log(`newBookRef: %o`, newBookRef)
+            return [...existingBooks, newBookRef]
+          },
+        },
+      })
+    },
   })
   const [resetBooks] = useMutation(RESET_BOOKS, {
     refetchQueries,
@@ -135,7 +162,37 @@ export function MutationsManualCacheUpdate() {
 function Docs() {
   return (
     <>
-      <PageParagraph>Manual cache update</PageParagraph>
+      <PageParagraph>
+        This example is showing how to update the cache manually after a
+        mutation with the{' '}
+        <Link
+          href="https://www.apollographql.com/docs/react/data/mutations/#the-update-function"
+          target="_blank"
+        >
+          <code>update</code>
+        </Link>{' '}
+        function. This can be necessary when the automatic cache update that
+        happens after a mutation response isn't sufficient to update all
+        appropriate fields, this can happen for example in list fields.
+      </PageParagraph>
+      <PageParagraph>
+        <H2Inline>Updating of related queries:</H2Inline> Note how the books
+        query automatically updates in response to the cache data we set inside{' '}
+        <code>update</code> after the mutation.
+      </PageParagraph>
+      <PageParagraph>
+        <H2Inline>Refetch after update:</H2Inline> You do still have the option
+        to{' '}
+        <Link
+          href="https://www.apollographql.com/docs/react/data/mutations/#refetching-after-update"
+          target="_blank"
+        >
+          refetch after <code>update</code>
+        </Link>{' '}
+        if you'd like to make doubly sure everything is in sync. In this case
+        the refetch would overwrite local changes made if the data was
+        different.
+      </PageParagraph>
     </>
   )
 }
