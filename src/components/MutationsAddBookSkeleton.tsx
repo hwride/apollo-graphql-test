cd6d354b096a17af5bf38a442eb7f1c2cbfac9a5
Hugo Wride <hugowride@gmail.com>
2023-07-03 11:07:34 +0100

    Share mutations code.

---
+++ src/components/MutationsAddBookSkeleton.tsx
@@ -0,0 +1,134 @@
+import { gql } from '@apollo/client'
+import { Books } from '../components/Books.tsx'
+import { BorderButton } from '../components/ui/Button.tsx'
+import { ControlGrid } from '../components/ui/ControlGrid.tsx'
+import { BoolLabelledSelect } from '../components/ui/LabelledSelect.tsx'
+
+export const GET_BOOKS = gql`
+  query GetBooks {
+    books {
+      id
+      title
+      author
+    }
+  }
+`
+export const ADD_BOOK = gql`
+  mutation AddBook($title: String!, $author: String!) {
+    addBook(title: $title, author: $author) {
+      success
+      code
+      book {
+        id
+        title
+        author
+      }
+    }
+  }
+`
+export const RESET_BOOKS = gql`
+  mutation ResetBooks {
+    resetBooks {
+      success
+      code
+      books {
+        id
+        title
+        author
+      }
+    }
+  }
+`
+
+let nextBookTitle = 1
+function getNextBookTitleSuffix() {
+  return nextBookTitle++
+}
+
+export function MutationsAddBookSkeleton({
+  getBooksQuery,
+  addBookMutation,
+  resetBooksMutation,
+  refetchGetBooks,
+  setRefetchGetBooks,
+}: {
+  getBooksQuery: any
+  addBookMutation: any
+  resetBooksMutation: any
+  refetchGetBooks: any
+  setRefetchGetBooks: any
+}) {
+  const { data: booksData, loading: getBooksLoading } = getBooksQuery
+  const [addBook, { data, loading, error, reset }] = addBookMutation
+  const [resetBooks] = resetBooksMutation
+
+  console.group('Mutations render')
+  console.log(
+    `getBooks - loading: ${getBooksLoading}, booksData: %o`,
+    booksData
+  )
+  console.log(`addBook - loading: ${loading}, data: %o, error: %o`, data, error)
+  console.groupEnd()
+
+  return (
+    <>
+      <ControlGrid>
+        <BoolLabelledSelect
+          label={
+            <>
+              Refetch <code>GetBooks</code> after mutations
+            </>
+          }
+          value={refetchGetBooks}
+          onOptionChange={setRefetchGetBooks}
+        />
+      </ControlGrid>
+      <div className="mx-auto my-4 flex w-fit flex-col items-center gap-1">
+        <BorderButton
+          className="block"
+          onClick={async () =>
+            addBook({
+              variables: {
+                title: 'New book title ' + getNextBookTitleSuffix(),
+                author: 'New book author',
+              },
+            })
+          }
+        >
+          Add book
+        </BorderButton>
+        <BorderButton className="block" onClick={reset}>
+          <code>useMutation().reset()</code>
+        </BorderButton>
+        <BorderButton className="block" onClick={() => resetBooks()}>
+          Reset books
+        </BorderButton>
+      </div>
+      <div className="mx-auto w-fit">
+        <h2 className="font-bold">Mutation results</h2>
+        <dl className="my-2 grid w-[500px] grid-cols-2">
+          <dt>
+            <code>loading</code>
+          </dt>
+          <dd>{JSON.stringify(loading)}</dd>
+          <dt>
+            <code>error</code>
+          </dt>
+          <dd>{error === undefined ? 'undefined' : error.message}</dd>
+          <dt>
+            <code>data</code>
+          </dt>
+          <dd>
+            {data === undefined ? (
+              'undefined'
+            ) : (
+              <code>{`{ title: ${data.addBook.book.title} }`}</code>
+            )}
+          </dd>
+        </dl>
+      </div>
+
+      <Books booksData={booksData} />
+    </>
+  )
+}
--- src/examples/MutationsManualCacheUpdate.tsx
+++ src/examples/MutationsManualCacheUpdate.tsx
@@ -1,9 +1,11 @@
 import { gql, useMutation, useQuery } from '@apollo/client'
 import { useState } from 'react'
-import { Books } from '../components/Books.tsx'
-import { BorderButton } from '../components/ui/Button.tsx'
-import { ControlGrid } from '../components/ui/ControlGrid.tsx'
-import { BoolLabelledSelect } from '../components/ui/LabelledSelect.tsx'
+import {
+  ADD_BOOK,
+  GET_BOOKS,
+  MutationsAddBookSkeleton,
+  RESET_BOOKS,
+} from '../components/MutationsAddBookSkeleton.tsx'
 import { Link } from '../components/ui/Link.tsx'
 import { Page } from '../components/ui/Page.tsx'
 import {
@@ -11,52 +13,11 @@ import {
   PageParagraph,
 } from '../components/ui/PageParagraph.tsx'
 
-const GET_BOOKS = gql`
-  query GetBooks {
-    books {
-      id
-      title
-      author
-    }
-  }
-`
-const ADD_BOOK = gql`
-  mutation AddBook($title: String!, $author: String!) {
-    addBook(title: $title, author: $author) {
-      success
-      code
-      book {
-        id
-        title
-        author
-      }
-    }
-  }
-`
-const RESET_BOOKS = gql`
-  mutation ResetBooks {
-    resetBooks {
-      success
-      code
-      books {
-        id
-        title
-        author
-      }
-    }
-  }
-`
-
-let nextBookTitle = 1
-function getNextBookTitleSuffix() {
-  return nextBookTitle++
-}
-
 export function MutationsManualCacheUpdate() {
-  const { data: booksData, loading: getBooksLoading } = useQuery(GET_BOOKS)
+  const getBooksQuery = useQuery(GET_BOOKS)
   const [refetchGetBooks, setRefetchGetBooks] = useState(false)
   const refetchQueries = refetchGetBooks ? [GET_BOOKS] : undefined
-  const [addBook, { data, loading, error, reset }] = useMutation(ADD_BOOK, {
+  const addBookMutation = useMutation(ADD_BOOK, {
     refetchQueries,
     // Use update to manually modify the cache after the mutation completes and
     // add the new book.
@@ -84,79 +45,21 @@ export function MutationsManualCacheUpdate() {
       })
     },
   })
-  const [resetBooks] = useMutation(RESET_BOOKS, {
+  const resetBooksMutation = useMutation(RESET_BOOKS, {
     refetchQueries,
   })
-
-  console.group('Mutations render')
-  console.log(
-    `getBooks - loading: ${getBooksLoading}, booksData: %o`,
-    booksData
-  )
-  console.log(`addBook - loading: ${loading}, data: %o, error: %o`, data, error)
-  console.groupEnd()
 
   return (
     <Page title="Mutations - manual cache update">
       <Docs />
 
-      <ControlGrid>
-        <BoolLabelledSelect
-          label={
-            <>
-              Refetch <code>GetBooks</code> after mutations
-            </>
-          }
-          value={refetchGetBooks}
-          onOptionChange={setRefetchGetBooks}
-        />
-      </ControlGrid>
-      <div className="mx-auto my-4 flex w-fit flex-col items-center gap-1">
-        <BorderButton
-          className="block"
-          onClick={async () =>
-            addBook({
-              variables: {
-                title: 'New book title ' + getNextBookTitleSuffix(),
-                author: 'New book author',
-              },
-            })
-          }
-        >
-          Add book
-        </BorderButton>
-        <BorderButton className="block" onClick={reset}>
-          <code>useMutation().reset()</code>
-        </BorderButton>
-        <BorderButton className="block" onClick={() => resetBooks()}>
-          Reset books
-        </BorderButton>
-      </div>
-      <div className="mx-auto w-fit">
-        <h2 className="font-bold">Mutation results</h2>
-        <dl className="my-2 grid w-[500px] grid-cols-2">
-          <dt>
-            <code>loading</code>
-          </dt>
-          <dd>{JSON.stringify(loading)}</dd>
-          <dt>
-            <code>error</code>
-          </dt>
-          <dd>{error === undefined ? 'undefined' : error.message}</dd>
-          <dt>
-            <code>data</code>
-          </dt>
-          <dd>
-            {data === undefined ? (
-              'undefined'
-            ) : (
-              <code>{`{ title: ${data.addBook.book.title} }`}</code>
-            )}
-          </dd>
-        </dl>
-      </div>
-
-      <Books booksData={booksData} />
+      <MutationsAddBookSkeleton
+        getBooksQuery={getBooksQuery}
+        addBookMutation={addBookMutation}
+        resetBooksMutation={resetBooksMutation}
+        refetchGetBooks={refetchGetBooks}
+        setRefetchGetBooks={setRefetchGetBooks}
+      />
     </Page>
   )
 }
